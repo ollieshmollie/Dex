489026615a34921857c9002e3899bcc1dd718994
ollieshmollie <oliverduncan@icloud.com>
2016-10-22 20:29:08 -0500

    Update help messsage

--- lib/dex_manager.rb
+++ lib/dex_manager.rb
@@ -36,18 +36,19 @@ class DexManager
       -h                                Help
 
       add first last type num           Adds new contact
-      add -n first last                 Adds new name only
+      add -n first last                 Adds new name
       add -t index type num             Adds contact number
+      add -e index address              Adds contact email
       delete index                      Deletes contact
       delete -t index num_index         Deletes contact number
-      delete -e index email_index       Deletes contact email
+      delete -e index e_index           Deletes contact email
       edit -n index first last          Edits contact name
       edit -t index num_index type num  Edits contact number
-      edit -e index email_index email   Edits contact email
+      edit -e index e_index address     Edits contact email
       find -n <param>                   Searches by name (DEFAULT)
       find -t <param>                   Searches by number
       find -e <param>                   Searches by email
-    EOF
+      EOF
   end
 
   def run
