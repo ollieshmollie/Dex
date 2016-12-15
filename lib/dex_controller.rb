9e52302a761cb0253a964683e0d56dc46f0939ae
ollieshmollie <oliverduncan@icloud.com>
2016-12-14 21:40:52 -0600

    Rework find to be more flexible

--- lib/dex_controller.rb
+++ lib/dex_controller.rb
@@ -5,13 +5,13 @@ class DexController
   def initialize(args)
     @dex = Dex.new
     @options = {}
-    @version = "dex version 1.2"
+    @version = "dex version 2.0"
     @opt_parser = OptionParser.new do |opt|
       opt.banner = "Usage: dex [COMMAND] [OPTIONS] [ARGUMENTS]"
       opt.on('-n', '--name', 'Edit name') do
         @options[:name] = true
       end
-      opt.on('-t', '--phone', 'Edit phone number') do
+      opt.on('-p', '--phone', 'Edit phone number') do
         @options[:number] = true
       end
       opt.on('-e', '--email', 'Edit email') do
@@ -42,19 +42,15 @@ class DexController
       -h                                Help
 
       add first last                    Adds new name
-      add -t index type num             Adds contact number
+      add -p index type num             Adds contact number
       add -e index address              Adds contact email
       delete index                      Deletes contact
-      delete -t index num_index         Deletes contact number
+      delete -p index num_index         Deletes contact number
       delete -e index e_index           Deletes contact email
       edit -n index first last          Edits contact name
-      edit -t index num_index type num  Edits contact number
+      edit -p index num_index type num  Edits contact number
       edit -e index e_index address     Edits contact email
-      find <param>                      Searches by name (DEFAULT)
-      find -t <param>                   Searches by number
-      find -e <param>                   Searches by email
-      find -f <letter>                  Names by first name letter
-      find -l <letter>                  Names by last name letter
+      find <param>                      Search
       EOF
   end
 
@@ -68,22 +64,8 @@ class DexController
     case @command
 
     when "find"
-      if @options.empty? || @options[:name]
-        param = @args[0]
-        puts @dex.find_by_name(param)
-      elsif @options[:number]
-        param = @args[0]
-        puts @dex.find_by_number(param)
-      elsif @options[:email]
-        param = @args[0]
-        puts @dex.find_by_email(param)
-      elsif @options[:first]
-        param = @args[0]
-        puts @dex.find_by_first_name_letter(param)
-      elsif @options[:last]
-        param = @args[0]
-        puts @dex.find_by_last_name_letter(param)
-      end
+      param = @args[0]
+      puts @dex.find_by_name(param)
 
     when "add"
       if @options.empty?
@@ -150,13 +132,9 @@ class DexController
   def args_are_valid?
     case @command
     when "find"
-      return false if @args.count != 1
-      return false if @options.count > 1
-      return false if !@options.empty? && !@options[:name] && !@options[:number] && !@options[:email] && !@options[:first] && !@options[:last]
-      return false if (@options[:first] || @options[:last]) && @args[0].length != 1
+      return false if !@options.empty?
       return true
     when "add"
-      return false if @options.empty? && @args.count != 4
       return false if @options.count > 1
       return false if !@options.empty? && !@options[:name] && !@options[:number] && !@options[:email]
       return false if @options[:name] && @args.count != 2
