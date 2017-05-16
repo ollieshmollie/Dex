a9a25a13257eb549559a31f6586cf3f5ec3ee64e
AJ Bond <olishmollie@gmail.com>
2017-05-16 18:36:19 -0500

    One last try to run in prod

--- lib/tact/authorizable.rb
+++ lib/tact/authorizable.rb
@@ -1,13 +1,10 @@
-module Tact
-  module Authorizable
-    def authorize
-      system('oauth2l fetch --json client_secret.json https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/contacts.readonly')
-    end
+module Authorizable
+  def authorize
+    system('oauth2l fetch --json client_secret.json https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/contacts.readonly')
+  end
 
-    def authorized?
-      credentials = File.join(File.expand_path('~'), '.oauth2l.token')
-      File.exists?(credentials) && !File.zero?(credentials)
-    end
+  def authorized?
+    credentials = File.join(File.expand_path('~'), '.oauth2l.token')
+    File.exists?(credentials) && !File.zero?(credentials)
   end
 end
-
