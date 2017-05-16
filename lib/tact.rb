dfb7b5d4fdf2a461fc3328cd7462474d232fd873
AJ Bond <olishmollie@gmail.com>
2017-05-16 18:51:54 -0500

    Manually require lib files

--- lib/tact.rb
+++ lib/tact.rb
@@ -3,10 +3,17 @@ require 'active_record'
 require 'sqlite3'
 require 'colored'
 
-# recursively requires all files in ./lib and down that end in .rb
-Dir['./lib/tact/*.rb'].each do |file|
-  require file
-end
+# require lib files
+require 'authorizable'
+require 'card'
+require 'contact'
+require 'email'
+require 'google_client'
+require 'phone_number'
+require 'rolodex'
+require 'tact'
+require 'version'
+
 
 APP_ROOT ||= File.join(File.dirname(__FILE__), '../')
 DEV_DB ||= File.join(File.expand_path('~'), '.tact', 'tact.sqlite3')
@@ -25,5 +32,3 @@ else
     :database => DEV_DB 
   )
 end
-
-require 'tact'
