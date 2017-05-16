102d637fff57d42b34a9816c45fc399b1f9e41e1
AJ Bond <olishmollie@gmail.com>
2017-05-16 18:21:21 -0500

    Try fixing auth module

--- lib/tact/authorizable.rb
+++ lib/tact/authorizable.rb
@@ -1,13 +1,13 @@
-module Authorizable
-  def authorize
-    system('oauth2l fetch --json client_secret.json https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/contacts.readonly')
-  end
+module Tact
+  module Authorizable
+    def authorize
+      system('oauth2l fetch --json client_secret.json https://www.googleapis.com/auth/contacts https://www.googleapis.com/auth/contacts.readonly')
+    end
 
-  def authorized?
-    credentials = File.join(File.expand_path('~'), '.oauth2l.token')
-    File.exists?(credentials) && !File.zero?(credentials)
+    def authorized?
+      credentials = File.join(File.expand_path('~'), '.oauth2l.token')
+      File.exists?(credentials) && !File.zero?(credentials)
+    end
   end
 end
-
-
 
